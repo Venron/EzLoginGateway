950db24ae22c8d7c8ac4e9a357e37a8a77a30f78
Marc Friedrich <marc.friedrich94@web.de>
2017-06-06 13:09:50 +0200

    JsonGen

--- src/com/ezlogin/gateway_data/JsonGen.java
+++ src/com/ezlogin/gateway_data/JsonGen.java
@@ -1,8 +1,9 @@
 package com.ezlogin.gateway_data;
 
-import com.ezlogin.log.LogStamp;
-import com.google.gson.JsonObject;
 import com.sun.istack.internal.NotNull;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
@@ -11,6 +12,8 @@ import java.util.Date;
  * Created by marcf on 05.06.2017.
  */
 public class JsonGen {
+
+    private static final String JSON_PARSE_ERROR = "json_parse_error";
 
     private static SimpleDateFormat sdf = new SimpleDateFormat("HH:mm:ss");
 
@@ -22,11 +25,36 @@ public class JsonGen {
         return JsonGen.getTime() + log + "\n";
     }
 
-    public static String prepClientLogin(String email, String hash, String sToken) {
-        JsonObject o = new JsonObject();
-        o.addProperty("email", email);
-        o.addProperty("hash", hash);
-        o.addProperty("token", sToken);
-        return o.toString();
+    /*JSON Request from Client*/
+    public static String prepClientLogin(String action, String email, String hash, String sToken) {
+        JSONObject o = new JSONObject();
+        o.put("action", action);
+        o.put("email", email);
+        o.put("hash", hash);
+        o.put("token", sToken);
+        return o.toJSONString();
+    }
+
+    /*User login request gets transformed */
+    public static String prepClientLoginGateway(String clientRequest, String action, String masterToken) {
+        String email;
+        String hash;
+        JSONParser p = new JSONParser();
+        JSONObject jObject = null;
+        try {
+            jObject = (JSONObject) p.parse(clientRequest);
+        } catch (ParseException e) {
+            return JsonGen.JSON_PARSE_ERROR;
+        }
+
+        email = (String) jObject.get("email");
+        hash = (String) jObject.get("hash");
+
+        JSONObject o = new JSONObject();
+        o.put("action", action);
+        o.put("email", email);
+        o.put("hash", hash);
+        o.put("master-token", masterToken);
+        return o.toJSONString();
     }
 }
